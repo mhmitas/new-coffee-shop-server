5df166bc655db30b982ae59aa7a477d14ceba361
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-24 16:38:48 +0600

    commit -

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(express.json());
 //NBPVqIVxKo1NVr8z;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb://localhost:27017`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
